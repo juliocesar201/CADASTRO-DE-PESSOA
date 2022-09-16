fafc41356dbe8c837dba7357aa59639f32aa8411
JÚLIO CÉSAR MIRANDA GOMES <79476210+juliocesar201@users.noreply.github.com>
2022-09-16 13:07:07 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'formulario';
+
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexão->connect_errno)
+//{
+//        echo "erro";
+//}
+//else
+//{
+//    echo "Conexão efetuada";
+//}
+?>
